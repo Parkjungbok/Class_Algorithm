bad03987fe1711c2d2a4ab9dfe2b3404bd9af77a
Jungbok <sumeing.vogi@gmail.com>
2024-01-17 17:56:08 +0900

    feat : 240117과제연습

---
+++ 240117.homework/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _240117.homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
+            int d;
+            
+            pq.Enqueue(60, 4);
+            pq.Enqueue(40, 4);
+            pq.Enqueue(20, 1);
+            pq.Enqueue(50, 2);
+            pq.Enqueue(30, 3);
+            pq.Enqueue(10, 4);
+            pq.Enqueue(5, 6);
+            // 일주일 체크
+            for ( d = 0; d < 7; d++)
+            {
+                
+            }
+
+            
+            
+
+        }
+    }
+}
