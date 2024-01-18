b45810faad6367774a6b0018d0a1fdba022b9a5e
Jungbok <sumeing.vogi@gmail.com>
2024-01-18 18:01:27 +0900

    feat : 치트키 과제

--- 240118.homework/Program.cs
+++ 240118.homework/Program.cs
@@ -1,32 +1,33 @@
 ﻿namespace _240118.homework
 {
-    internal class Program
+    public class CheetKey
     {
         private Dictionary<string, Action> cheatDic;
-        
-        public class cheat()
+            
+        public CheetKey()
         {
+            cheatDic = new Dictionary<string, Action>();
 
-        }
-
+            cheatDic.Add("show me the money", ShowMeTheMoney);
+            cheatDic.Add("there is no cow level", ThereIsNoCowLevel);
+        }        
         public void Run(string cheatkey)
         {
             // 조건문 없이 바로 탐색
-            
+            cheatDic.TryGetValue(cheatkey, out Action action);
+            action?.Invoke();
         }
-
         public void ShowMeTheMoney()
         {
             Console.WriteLine("골드를 늘려주는 치트키 발동!");
         }
-
         public void ThereIsNoCowLevel()
         {
             Console.WriteLine("바로 승리합니다 치트키 발동!");
         }
-
         static void Main(string[] args)
         {
+
 
         }
     }
