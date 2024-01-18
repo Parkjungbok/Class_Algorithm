e432bfe3543e1dcd28079c54a3572eeecee3e2fb
Jungbok <sumeing.vogi@gmail.com>
2024-01-18 17:23:00 +0900

    feat : 과제

---
+++ 240118.homework/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _240118.homework
+{
+    internal class Program
+    {
+        private Dictionary<string, Action> cheatDic;
+        
+        public class cheat()
+        {
+
+        }
+
+        public void Run(string cheatkey)
+        {
+            // 조건문 없이 바로 탐색
+            
+        }
+
+        public void ShowMeTheMoney()
+        {
+            Console.WriteLine("골드를 늘려주는 치트키 발동!");
+        }
+
+        public void ThereIsNoCowLevel()
+        {
+            Console.WriteLine("바로 승리합니다 치트키 발동!");
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
