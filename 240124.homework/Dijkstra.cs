b74d091fa4347d61c0228995c25fc39682e50387
Jungbok <sumeing.vogi@gmail.com>
2024-01-24 17:50:32 +0900

    feat : 수업연습

---
+++ 240124.homework/Dijkstra.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _240124.homework
+{
+    internal class Dijkstra
+    {
+        const int INF = 99999;
+        public static void shotpath(int[,] graph2, int start, out bool[] visited, out int[] parent, out int[] distance)
+        {
+            int size = graph2.GetLength(0);
+            visited = new bool[size];
+            parent = new int[size];
+            distance = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                visited[i] = false;
+                distance[i] = INF;
+                parent[i] = -1;
+            }
+            distance[start] = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                
+            }
+        }
+    }
+}
---
+++ 240124.homework/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _240124.homework
+{
+    internal class Program
+    {
+        private static void PrintDijkstra(int[] distance, int[] path)
+        {
+            Console.WriteLine($"{"Vertex",8}{"Visit",8}{"Path",8}");
+
+            for (int i = 0; i < distance.Length; i++)
+            {
+                Console.Write($"{i,8}");
+
+                if (distance[i] >= INF)
+                    Console.Write($"{"INF",8}");
+                else
+                    Console.Write($"{distance[i],8}");
+
+                if (path[i] < 0)
+                    Console.WriteLine($"{"X",8}");
+                else
+                    Console.WriteLine($"{path[i],8}");
+            }
+        }
+
+        const int INF = 99999;
+        static void Main(string[] args)
+        {
+            int[,] graph2 =
+            {//    0    1    2    3    4    5    6     
+                { INF, INF, INF, INF, INF, INF, INF },
+                { INF, INF, INF, INF, INF, INF, INF },
+                { INF, INF, INF, INF, INF, INF, INF },
+                { INF, INF, INF, INF, INF, INF, INF },
+                { INF, INF, INF, INF, INF, INF, INF },
+                { INF, INF, INF, INF, INF, INF, INF },
+                { INF, INF, INF, INF, INF, INF, INF },
+            };
+
+            Dijkstra.shotpath(graph2, 0, out bool[] visited, out int[] distance, out int[] parents);
+
+            Console.WriteLine("<Dijkstra>");
+            PrintDijkstra(distance, parents);
+        }
+    }
+}
